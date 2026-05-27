393e37f8f93379477e76c7e2322e81373cf19d7a
banzaoceane40-cloud <banzaoceane40@gmail.com>
2026-05-27 17:58:17 +0200

    variables created

---
+++ app/src/main/java/za/ac/iie/guessthegen/MainActivity.kt
@@ -0,0 +1,28 @@
+package za.ac.iie.guessthegen
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val editYear = findViewById<EditText>(R.id.yearTxtInput)
+        val buttonCheck = findViewById<Button>(R.id.searchButton)
+        val textResult = findViewById<TextView>(R.id.txtResult)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
